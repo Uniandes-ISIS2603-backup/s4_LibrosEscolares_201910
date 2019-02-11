931c89ec579c14a0b473635360a665a070c71315
nfJaramillo <nf.jaramillo@uniandes.edu.co>
2019-02-11 12:44:06 -0500

    Usuario DTO, parcial

---
+++ s4_libros-api/src/main/java/co/edu/uniandes/csw/libros/dtos/UsuarioDTO.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.libros.dtos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author estudiante
+ */
+public class UsuarioDTO implements Serializable {
+
+    private String correo;
+    private String nombreUsuario;
+    private int calificacion;
+
+    
+    public UsuarioDTO()
+    {
+        
+    }
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the nombreUsuario
+     */
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    /**
+     * @param nombreUsuario the nombreUsuario to set
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+}
