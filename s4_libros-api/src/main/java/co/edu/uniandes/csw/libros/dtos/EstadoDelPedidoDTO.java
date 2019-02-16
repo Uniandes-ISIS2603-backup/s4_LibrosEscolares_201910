0c0f1b3f18d740468ea95ba0d7da440f3a119087
estudiante <estudiante@ISIS2603-499.sis.virtual.uniandes.edu.co>
2019-02-15 20:13:20 -0500

    DTOs Pedido-Respuesta

---
+++ s4_libros-api/src/main/java/co/edu/uniandes/csw/libros/dtos/EstadoDelPedidoDTO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.libros.dtos;
+
+/**
+ *
+ * @author estudiante
+ */
+public enum EstadoDelPedidoDTO  {
+    
+    ACEPTADO_POR_EL_VENDEDOR,
+    ENVIADO,
+    EN_REVISION,
+    RECIBIDO,
+    NO_ACEPTADO
+    
+}
