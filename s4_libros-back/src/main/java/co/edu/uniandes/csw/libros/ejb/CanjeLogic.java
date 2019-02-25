83d71e060a7968e8f713dc0c3481448818574729
wr.smith10 <wr.smith10@uniandes.edu.co>
2019-02-25 12:21:33 -0500

    CanjeLogic
    actualiza CanjeLogic con el metodo createCanje

--- s4_libros-back/src/main/java/co/edu/uniandes/csw/libros/ejb/CanjeLogic.java
+++ s4_libros-back/src/main/java/co/edu/uniandes/csw/libros/ejb/CanjeLogic.java
@@ -5,10 +5,26 @@
  */
 package co.edu.uniandes.csw.libros.ejb;
 
+import co.edu.uniandes.csw.libros.entities.CanjeEntity;
+import co.edu.uniandes.csw.libros.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.libros.persistence.CanjePersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
- * @author estudiante
+ * @author William Smith
  */
+
+@Stateless
 public class CanjeLogic {
+    
+    @Inject
+    private CanjePersistence persistence;
+    
+    public CanjeEntity createCanje(CanjeEntity canje){
+        canje=persistence.create(canje);
+        return canje;
+    }
     
 }
