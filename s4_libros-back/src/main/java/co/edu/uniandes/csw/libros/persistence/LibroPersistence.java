ea7f2eeac284544cd106ac333fb0274233b1f544
ma.munoza <ma.munoza@uniandes.edu.co>
2019-03-08 20:22:25 -0500

    Avances

--- s4_libros-back/src/main/java/co/edu/uniandes/csw/libros/persistence/LibroPersistence.java
+++ s4_libros-back/src/main/java/co/edu/uniandes/csw/libros/persistence/LibroPersistence.java
@@ -18,51 +18,57 @@ import javax.persistence.TypedQuery;
  */
 @Stateless
 public class LibroPersistence {
-    @PersistenceContext(unitName="librosPU")
+
+    @PersistenceContext(unitName = "librosPU")
     protected EntityManager em;
-    
-    public LibroEntity create (LibroEntity libroEntity){
-        
+
+    public LibroEntity create(LibroEntity libroEntity) {
+
         em.persist(libroEntity);
         return libroEntity;
     }
-    public LibroEntity find (Long libroId){
-  
+
+    public LibroEntity find(Long libroId) {
+
         return em.find(LibroEntity.class, libroId);
     }
-    public List<LibroEntity> findAll()
-    {
-       TypedQuery query= em.createQuery("select u from LibroEntity", LibroEntity.class);
-       return query.getResultList();
+
+    public List<LibroEntity> findAll() {
+        TypedQuery query = em.createQuery("select u from LibroEntity", LibroEntity.class);
+        return query.getResultList();
     }
-    
-    public  LibroEntity findByName(String titulo)
-    {
-         TypedQuery query= em.createQuery("select u from LibroEntity where u.titulo = :title", LibroEntity.class);
-         query= query.setParameter("title", titulo);
-         List<LibroEntity> names=query.getResultList();
-         LibroEntity resultado=null;
-         if(names!=null && !names.isEmpty())
-         {
-             resultado=names.get(0);
-         }
-         return resultado;
-    }
-    public void verificarISBN(String ISBN) throws Exception
-    {
-       
-        
-         String t= ISBN.replaceAll("-","");
-          int s=0;
-        for(int i=0; i<12;i++)
-        {
-            s+=Character.getNumericValue(t.charAt(i)) ;
+
+    public LibroEntity findByName(String titulo) {
+        TypedQuery query = em.createQuery("select u from LibroEntity where u.titulo = :title", LibroEntity.class);
+        query = query.setParameter("title", titulo);
+        List<LibroEntity> names = query.getResultList();
+        LibroEntity resultado = null;
+        if (names != null && !names.isEmpty()) {
+            resultado = names.get(0);
         }
-        int u= Character.getNumericValue(t.charAt(12));
-         if(t.length()!=13||s%u!=0)
-        {
+        return resultado;
+    }
+
+    public void verificarISBN(String ISBN) throws Exception {
+
+        String t = ISBN.replaceAll("-", "");
+        int s = 0;
+        for (int i = 0; i < 12; i++) {
+            s += Character.getNumericValue(t.charAt(i));
+        }
+        int u = Character.getNumericValue(t.charAt(12));
+        if (t.length() != 13 || s % u != 0) {
             throw new Exception();
         }
-       
+
+    }
+
+    public LibroEntity actualizar(LibroEntity libroEntity) {
+
+        return em.merge(libroEntity);
+    }
+
+    public void eliminar(long id) {
+        em.remove(id);
     }
 }
